51b1f3047dadc8f9f03ca7b2624db4853d084556
Tarık BAĞRIYANIK <tbagriyanik@gmail.com>
2026-07-17 12:53:09 +0300

    chore: configure deployment and update dependencies
    Add gh-pages for deployment, adjust vite base path to support local development and production, and clean up devDependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig(({ command }) => {
   return {
-    base: '/otosay/',
+    base: command === 'build' ? '/otosay/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
